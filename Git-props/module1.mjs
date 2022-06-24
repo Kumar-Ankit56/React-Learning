2bca0e15ff8ef797b8f8c9e1af4cb738dce83ec9
Ankit kumar <73521123+Kumar-Ankit56@users.noreply.github.com>
2022-06-24 13:38:05 +0530

    Add files via upload

---
+++ Git-props/module1.mjs
@@ -0,0 +1,11 @@
+/*import ui from './module2.mjs'
+console.log(ui);
+*/
+
+import ui, {a,b,c} from './module2.mjs'
+console.log(ui);
+console.log(a);
+console.log(b);
+console.log(c);
+
+//Here other name we are exporting but it is not exporting in the default mode so we need to manully import it.
---
+++ Git-props/module2.mjs
@@ -0,0 +1,10 @@
+const a="harry";
+const b="Rohan";
+const c="Aakash";
+const d="Priyanka";
+
+export default d; 
+// here we are exporting the bideafult so it will come by anyhow.
+export {a};
+export {b};
+export {c};
